47e1f9b91a3d222f8e11f5cc64e4eac5d0257d0f
Malaxiz <malaxiz.main@gmail.com>
2016-02-28 23:43:10 +0100

    Added app info and version checking

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,32 @@
 angular.module('mat')
 .controller('MainCtrl',
-	function($scope, $firebaseArray) {
+	function($scope, $firebaseArray, $ionicPopup, $timeout) {
+
+		$scope.version = '1.0';
+		$scope.isOldVersion = false;
+		$scope.realVersion = '';
+
+		$scope.infoPopup = function() {
+			$ionicPopup.alert({
+				template: '<ion-list>\
+								<ion-item ng-click="openLink(\'http://ktcteknik.se/ktcdimu/ktcmat.apk\');">\
+									<a ng-class="{\'old-version-popup\': isOldVersion, \'current-version-popup\': !isOldVersion}">Update App (apk-file)</a>\
+									</ion-item>\
+								<ion-item ng-click="openLink(\'https://github.com/Malaxiz/KTCMat\');">\
+									<a>Source Code</a>\
+								</ion-item>\
+							</ion-list>',
+				title: 'App Info',
+				scope: $scope,
+			});
+		}
+
+		// openLink('https://github.com/Malaxiz/KTCMat');
+
+		$scope.openLink = function(link) {
+			window.open(link, '_system', 'location=yes');
+			return false;
+		}
 
 		Date.prototype.yyyymmdd = function() {
 			var yyyy = this.getFullYear().toString();
@@ -33,10 +59,44 @@ angular.module('mat')
 			return tmp;
 		}
 
+		$scope.versionCompare = function(a, b) {
+		    if (a === b) {
+		       return 0;
+		    }
+		    var a_components = a.split(".");
+		    var b_components = b.split(".");
+		    var len = Math.min(a_components.length, b_components.length);
+
+		    for (var i = 0; i < len; i++) {
+		        if (parseInt(a_components[i]) > parseInt(b_components[i])) {
+		            return 1;
+		        }
+		        if (parseInt(a_components[i]) < parseInt(b_components[i])) {
+		            return -1;
+		        }
+		    }
+		    if (a_components.length > b_components.length) {
+		        return 1;
+		    }
+		    if (a_components.length < b_components.length) {
+		        return -1;
+		    }
+		    return 0;
+		}
+
 		$scope.todayDate = (new Date()).yyyymmdd();
 
 		var ref = new Firebase('https://kholm-mat.firebaseio.com/mat');
 		$scope.weeks = $firebaseArray(ref);
 		$scope.days = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag'];
+
+		$scope.weeks.$loaded(function() {
+			$scope.realVersion = $scope.weeks[0]['version'];
+			var comparison = $scope.versionCompare($scope.realVersion, $scope.version);
+			if(comparison == 1) { // There is a newer version
+				console.log('this is an old version');
+				$scope.isOldVersion = true;
+			}
+		});
 	}
 )
